6464ae910531f3d53341f01e5824b5b38bb263ba
League <League@imac-3.attlocal.net>
2018-07-06 16:53:30 -0700

    ednfhgbak.

--- src/MyFirstSwingGUI.java
+++ src/MyFirstSwingGUI.java
@@ -15,25 +15,26 @@ import javax.swing.JPanel;
 public class MyFirstSwingGUI {
 	public static void main(String[] args) {
 		//1. Create and initialize an object of the JFrame class
-
+		JFrame Alpine = new JFrame();
 		//2. Set your JFrame object to be visible
-
+		Alpine.setVisible(true);
 		//3. Run your program. Do you see your window? It's probably very small.
-		
+		Alpine.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 		//4. Set the default close operation to JFrame.EXIT_ON_CLOSE
-
+		
 		//5. Create and initialize an object of the JPanel class
-
+		JPanel Dulzura = new JPanel();
 		//6. Create and initialize an object of the JLabel class
-
+		JLabel Pendleton = new JLabel();
 		//7. Set the text of the JLabel to a lovely greeting.
-
+		Pendleton.setText("Good Morning!");
 		//8. Add the JPanel object to the JFrame
-
+		Alpine.add(Dulzura);
 		//9. Add the JLabel object to the JPanel
-
+		Dulzura.add(Pendleton);
 		//10. Pack your JFrame.
-
+		Alpine.pack();
 		//11. Run your program again. Do you see your message.
 		
 		//12. Use the loadImage method to set the icon of the JLabel object.  
